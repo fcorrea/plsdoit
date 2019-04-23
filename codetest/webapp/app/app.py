481048ec3c409725b3de0b239a69608a3319d76e
Fernando Correa Neto <fcdoth@gmail.com>
2019-04-23 13:30:17 -0300

    get client and priority to render

--- codetest/webapp/app/app.py
+++ codetest/webapp/app/app.py
@@ -1,36 +1,54 @@
 from flask import render_template, request
-from flask_wtf import FlaskForm
-from wtforms import (
-    StringField,
-    DateTimeField,
-    TextAreaField,
-    SubmitField,
-    BooleanField,
-    PasswordField,
-)
-from wtforms.validators import DataRequired, Length
+
 from flask_bootstrap import Bootstrap
 
-from .models import db, FeatureRequest
+from wtforms import DateTimeField, SubmitField, SelectField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length
+from wtforms_alchemy import model_form_factory, ModelFormField, QuerySelectField
+from wtforms_alchemy.utils import choice_type_coerce_factory
+
+from .models import db, FeatureRequest, Client, Priority
 from . import create_app
+
+BaseModelForm = model_form_factory(FlaskForm)
+
+
+class ModelForm(BaseModelForm):
+    @classmethod
+    def get_session(self):
+        return db.session
+
 
 app = create_app()
 
 bootstrap = Bootstrap(app)
 
 
-class RequestFeatureForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired(), Length(1, 20)])
-    description = TextAreaField(
-        "Description", validators=[DataRequired(), Length(8, 150)]
-    )
+class RequestFeatureForm(ModelForm):
+    class Meta:
+        model = FeatureRequest
+
     target_date = DateTimeField("Target Date", id="datepick")
+    client = QuerySelectField(
+        query_factory=lambda: Client.query.all(),
+        get_pk=lambda a: a.id,
+        get_label=lambda a: a.name,
+        allow_blank=False,
+        blank_text=u"-- please choose --",
+    )
+    priority = SelectField(
+        choices=Priority.PRIORITIES,
+        coerce=choice_type_coerce_factory(Priority.value.type),
+        validators=[DataRequired()],
+    )
     submit = SubmitField()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    form = RequestFeatureForm()
+    return render_template("index.html", form=form)
 
 
 @app.route("/form")
--- codetest/webapp/app/models.py
+++ codetest/webapp/app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import flask_sqlalchemy
+from sqlalchemy_utils import ChoiceType
 
 db = flask_sqlalchemy.SQLAlchemy()
 
@@ -13,8 +14,10 @@ class Client(db.Model):
 
 class Priority(db.Model):
 
+    PRIORITIES = [(1, u"Very High"), (2, u"High"), (3, u"Medium"), (4, u"Low")]
+
     id = db.Column(db.Integer, primary_key=True)
-    value = db.Column(db.Integer, nullable=False)
+    value = db.Column(ChoiceType(PRIORITIES))
 
 
 class ProductArea(db.Model):
@@ -26,20 +29,22 @@ class ProductArea(db.Model):
 class FeatureRequest(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), unique=True, nullable=False)
-    description = db.Column(db.Text(), nullable=False)
-    target_date = db.Column(db.DateTime, nullable=False)
+    title = db.Column(
+        db.String(200), unique=True, nullable=False, info={"label": "Title"}
+    )
+    description = db.Column(db.Text(), nullable=False, info={"label": "Description"})
+    target_date = db.Column(db.DateTime, nullable=False, info={"label": "Target Date"})
 
-    client_id = db.Column(db.Integer, db.ForeignKey("client.id"), nullable=False)
-    client = db.relationship(Client, backref=db.backref("feature_requests", lazy=True))
+    client_id = db.Column(db.Integer, db.ForeignKey(Client.id), nullable=False)
+    client = db.relationship(Client, backref=db.backref("feature_request", lazy=True))
     client_priority_id = db.Column(
-        db.Integer, db.ForeignKey("priority.id"), nullable=False
+        db.Integer, db.ForeignKey(Priority.id), nullable=False
     )
     client_priority = db.relationship(
         Priority, backref=db.backref("priority", lazy=True)
     )
     product_area_id = db.Column(
-        db.Integer, db.ForeignKey("product_area.id"), nullable=False
+        db.Integer, db.ForeignKey(ProductArea.id), nullable=False
     )
     product_area = db.relationship(
         ProductArea, backref=db.backref("product_area", lazy=True)
