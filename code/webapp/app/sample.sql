c89aeaeaa41c9197fa5115ce4cb29e12ec8c5df0
Fernando Correa Neto <fcdoth@gmail.com>
2019-05-07 21:23:29 -0300

    remote old sql script

--- code/webapp/app/sample.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO feature_request (title, description, target_date, client_priority, client_id, product_area_id) VALUES ('Feature request 1', 'A nice feature request', NOW(), 1, 1, 2);
-INSERT INTO feature_request (title, description, target_date, client_priority, client_id, product_area_id) VALUES ('Feature request 2', 'A nice feature request', NOW(), 2, 2, 2);
-INSERT INTO feature_request (title, description, target_date, client_priority, client_id, product_area_id) VALUES ('Feature request 3', 'A nice feature request', NOW(), 1, 2, 3);
-INSERT INTO feature_request (title, description, target_date, client_priority, client_id, product_area_id) VALUES ('Feature request 4', 'A nice feature request', NOW(), 1, 3, 2);
